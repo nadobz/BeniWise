f32ac536fb03cb2524966ebc296baf6e29e7b2df
Dannielyn Obille <elynobille@gmail.com>
2026-08-02 19:54:11 +0800

    created appdbcontect [not yet done!!]

---
+++ BeniWise.DataModel/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeniWise.DataModel
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+               "Server=OSI-PC173\\SQLEXPRESS;"
+               + "Database=BeniWise;"
+               + "Integrated Security=true;"
+               + "TrustServerCertificate=true;");
+        }
+    }
+}
