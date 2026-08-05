b088e50bce8de63ac59d1981a75442f3d8d86f51
Dannielyn Obille <elynobille@gmail.com>
2026-08-05 15:01:44 +0800

    Update database configuration

--- BeniWise.DataModel/AppDbContext.cs
+++ BeniWise.DataModel/AppDbContext.cs
@@ -11,28 +11,5 @@ namespace BeniWise.DataModel
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<MenuItem> MenuItems { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            // DAN
-            optionsBuilder.UseSqlServer(
-            "Server=OSI-PC173\\SQLEXPRESS;"
-            + "Database=BeniWise;"
-            + "Integrated Security=true;"
-            + "TrustServerCertificate=true;");
-            // ALGY
-            //  optionsBuilder.UseSqlServer(
-            //  "Server=localhost\\SQLEXPRESS;"
-            //  + "Database=BeniWise;"
-            //  + "Integrated Security=true;"
-            //  + "TrustServerCertificate=true;");
-            // DAPHNE
-            //   optionsBuilder.UseSqlServer(
-            //   "Server=localhost\\SQLEXPRESS;"
-            //   + "Database=BeniWise;"
-            //   + "Integrated Security=true;"
-            //   + "TrustServerCertificate=true;");
-
-
-        }
     }
 }
--- BeniWise.WebApp/Program.cs
+++ BeniWise.WebApp/Program.cs
@@ -1,11 +1,15 @@
 using BeniWise.DataModel;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
