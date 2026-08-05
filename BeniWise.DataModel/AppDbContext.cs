5f782ce74c8fddade3c2c2010cfe02e3b7ea78fe
Dannielyn Obille <elynobille@gmail.com>
2026-08-05 20:42:10 +0800

    Configure DbContext to use connection string

--- BeniWise.DataModel/AppDbContext.cs
+++ BeniWise.DataModel/AppDbContext.cs
@@ -7,9 +7,12 @@ namespace BeniWise.DataModel
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
 
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<MenuItem> MenuItems { get; set; } = null!;
-
     }
 }
